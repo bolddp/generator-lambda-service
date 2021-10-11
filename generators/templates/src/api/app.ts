2f3a0b8ae18b78fe7d8ec7e8e0bf6d3ae9814a83
Daniel Persson <daniel.persson@husqvarnagroup.com>
2021-10-11 23:30:45 +0200

    fixes

--- generators/templates/src/api/app.ts
+++ generators/templates/src/api/app.ts
@@ -3,8 +3,11 @@ import compression from 'compression';
 import cors from 'cors';
 import { errorHandlerFactory } from './ErrorHandler';
 import { sampleRouterFactory } from './routes/sampleRouter';
+import { reqHandler } from './routes/reqHandler';
 
 export default function (app = express()): express.Application {
+  const errorHandler = errorHandlerFactory.get();
+
   app.use(compression());
   app.use(cors());
   // Wrap the JSON body parsing in an exception handler
@@ -14,13 +17,20 @@ export default function (app = express()): express.Application {
       limit: '10mb',
     })(req, res, (err) => {
       if (err) {
-        errorHandlerFactory.get().handle(res, err);
+        errorHandler.handle(res, err);
       } else {
         next();
       }
     });
   });
   app.use(express.urlencoded({ extended: true }));
+
+  app.use(
+    '/alive',
+    reqHandler(errorHandler, async (req) => {
+      return { message: 'Hello from the API!' };
+    })
+  );
 
   // Attach routers to app REST API
   app.use('/samples', sampleRouterFactory.get());
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import Generator = require('yeoman-generator');
+import * as Generator from 'yeoman-generator';
 import { AppConfig, appConfigBuilder } from './appConfigBuilder';
-import { introduction } from './descriptions';
+import { getQuestions } from './getQuestions';
+import { footer, introduction } from './messages';
 import { packageJsonTransformer } from './packageJsonTransformer';
 
 export interface Answers {
@@ -28,107 +29,7 @@ module.exports = class extends Generator {
   async prompting() {
     this.log(introduction);
 
-    const questions: Generator.Question[] = [
-      {
-        type: 'input',
-        name: 'system',
-        message:
-          'System name - this will be the first part of the component names (e.g. "iot"). It can be left empty if you only have one system in your AWS account, which would make the system name prefix redundant:',
-        default: 'system',
-      },
-      {
-        type: 'input',
-        name: 'serviceName',
-        message:
-          'Service name - this will identify the components belonging to this service. It will be used right after the environment part in the component names:',
-        default: 'sample-service',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message:
-          'Description - a brief text that describes the service. This text will be used for describing the AWS Cloudformation stack and the generated package.json:',
-      },
-      {
-        type: 'number',
-        name: 'nodeJsRuntime',
-        message:
-          'Node.js runtime - what major version of the Node.js runtime should the service Lambdas use:',
-        default: 14,
-      },
-      {
-        type: 'input',
-        name: 'environment1',
-        message:
-          'Environment 1 (of 3) - a comma separated string containing the environment type, the AWS account id and the AWS region (e.g. "dev,123456789012,eu-west-1"):',
-      },
-      {
-        type: 'input',
-        name: 'environment2',
-        message:
-          'Environment 2 (of 3) - a comma separated string containing the environment type, the AWS account id and the AWS region (e.g. "dev,123456789012,eu-west-1"). Leave empty to skip:',
-      },
-      {
-        when: (a) => a.environment2,
-        type: 'input',
-        name: 'environment3',
-        message:
-          'Environment 3 (of 3) - a comma separated string containing the environment type, the AWS account id and the AWS region (e.g. "dev,123456789012,eu-west-1"). Leave empty to skip:',
-      },
-      {
-        type: 'confirm',
-        name: 'useScheduledHandler',
-        message:
-          'Scheduled Lambda - should the service include a Lambda that executes at fixed intervals:',
-        default: false,
-      },
-      {
-        when: (a) => a.useScheduledHandler,
-        type: 'input',
-        name: 'scheduledHandlerRate',
-        message:
-          'Scheduled Lambda - how often should it trigger (use syntax described here https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-create-rule-schedule.html):',
-        default: 'rate(1 minute)',
-      },
-      {
-        when: (a) => a.useScheduledHandler,
-        type: 'number',
-        name: 'scheduledHandlerMemorySize',
-        message:
-          'Scheduled Lambda - how much memory (Mb) should be allocated to the Lambda:',
-        default: 256,
-      },
-      {
-        when: (a) => a.useScheduledHandler,
-        type: 'number',
-        name: 'scheduledHandlerTimeout',
-        message:
-          'Scheduled Lambda - what timeout (seconds) should the Lambda have:',
-        default: 10,
-      },
-      {
-        type: 'confirm',
-        name: 'useApi',
-        message:
-          'API Lambda - should the service include a Lambda exposes a REST API:',
-        default: false,
-      },
-      {
-        when: (a) => a.useApi,
-        type: 'number',
-        name: 'apiMemorySize',
-        message:
-          'API Lambda - how much memory (Mb) should be allocated to the Lambda:',
-        default: 256,
-      },
-      {
-        when: (a) => a.useApi,
-        type: 'number',
-        name: 'apiTimeout',
-        message: 'API Lambda - what timeout (seconds) should the Lambda have:',
-        default: 10,
-      },
-    ];
+    const questions = getQuestions();
 
     const answers: Answers = await this.prompt<Answers>(questions);
     this.appConfig = appConfigBuilder(answers);
@@ -162,7 +63,7 @@ module.exports = class extends Generator {
     );
   }
 
-  // install() {
-  //   console.log(`Answers: ${JSON.stringify(this.appConfig)}`);
-  // }
+  end() {
+    this.log(footer(this.appConfig));
+  }
 };
